f524f5bc9cde6f7ddf7ca9dfe3b6fd0e505c09bd
Artem Shibakov <shibakow@gmail.com>
2025-10-06 13:44:47 +0400

    chore: add filter opts descriptions

--- src/types/datasets.ts
+++ src/types/datasets.ts
@@ -44,29 +44,80 @@ export interface UrlFilter extends UnknownRecord {
 }
 
 export interface ChatgptFilter extends UnknownRecord {
+    /**
+     * The prompt you want to search for, each prompt is
+     * a separate engagement with GPT and does not take into consideration
+     * past engagements that are not part of this prompt request.
+     * Note that there is a 4,096-character limit (for each input and output)
+     * according to the ChatGPT criteria
+     */
     prompt: string;
+    /**
+     * The additional_prompt is a follow-up input sent after receiving
+     * the first answer, aiming to clarify, expand, or refine
+     * the initial response. The system processes this prompt and returns
+     * the result as additional_answer_text.
+     */
     additional_prompt?: string;
+    /**
+     * Country from which to perform the search
+     */
     country?: string;
+    /**
+     * If it is set to 'true' and sources will not appear in the page
+     * you will receive an error message instead of the record
+     */
     require_sources?: boolean;
+    /**
+     * If set to true (default), the web search button will be clicked
+     * automatically; if set to false, the button will not be clicked.
+     */
     web_search?: boolean;
 }
 
 export interface LinkedinProfileFilter extends UnknownRecord {
-    first_name?: string;
-    last_name?: string;
+    /**
+     * First name of the person
+     */
+    first_name: string;
+    /**
+     * Last name of the person
+     */
+    last_name: string;
 }
 
 export interface LinkedinJobFilter extends UnknownRecord {
-    location?: string;
+    /**
+     * Collect jobs in a specific location
+     */
+    location: string;
+    /**
+     * Collect new jobs by keyword search like the job title,
+     * for example: Product Manager.
+     * Utilize quotation marks around specific words or phrases
+     * to ensure an exact match.
+     */
     keyword?: string;
+    /**
+     * Use country code with 2 letters like US or FR
+     */
     country?: string;
+    /**
+     * Time range of the job posting
+     */
     time_range?: 'Past 24 hours' | 'Past week' | 'Past month' | 'Any time';
+    /**
+     * Collect jobs from specific type like Full-time or Part-time
+     */
     job_type?:
         | 'Full-time'
         | 'Part-time'
         | 'Contract'
         | 'Temporary'
         | 'Volunteer';
+    /**
+     * Collect jobs from a specific experience level
+     */
     experience_level?:
         | 'Internship'
         | 'Entry level'
@@ -74,13 +125,52 @@ export interface LinkedinJobFilter extends UnknownRecord {
         | 'Mid-Senior level'
         | 'Director'
         | 'Executive';
+    /**
+     * Specify if you want to collect only "Remote", "On-site" or "Hybrid" jobs
+     */
     remote?: 'On-site' | 'Remote' | 'Hybrid';
+    /**
+     * Collect jobs from a specific company
+     */
     company?: string;
+    /**
+     * An area radius to find jobs within a certain distance
+     */
     location_radius?:
         | 'Exact location'
         | '5 miles (8 km)'
         | '10 miles (16 km)'
         | '25 miles (40 km)'
         | '50 miles (80 km)';
+    /**
+     * When set to true, the filter will exclude titles that
+     * do not contain the specified keywords
+     */
     selective_search?: boolean;
+}
+
+export interface AmazonCollectProductsFilter extends UnknownRecord {
+    /**
+     * Amazon product URL
+     */
+    url: string;
+    /**
+     * The ZIP code for the area you want to search from
+     */
+    zipcode?: string;
+    /**
+     * Language of the page
+     */
+    language?: string;
+}
+
+export interface AmazonCollectReviewsFilter extends UnknownRecord {
+    /**
+     * Product URL to get reviews from
+     */
+    url: string;
+    /**
+     * List of reviews IDs to exclude from collection
+     */
+    reviews_to_not_include?: string[];
 }
